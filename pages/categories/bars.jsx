a51babb6917ea2321b144fb4f886ed05a9f1f7cb
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2022-04-13 12:42:44 +0200

    Update bars.jsx

--- pages/categories/bars.jsx
+++ pages/categories/bars.jsx
@@ -5,7 +5,7 @@ const bars = () => {
     <div className="overflow-y-hidden">
       <div className="flex flex-col items-start justify-center py-12 px-4 md:px-6 lg:px-20 2xl:container 2xl:mx-auto">
         <h1 className="text-xl font-medium leading-6 text-gray-800 md:text-2xl md:leading-8 lg:text-4xl lg:leading-9">
-          Alimunium Burglar Bars
+          Aluminium Burglar Bars
         </h1>
         <div className="mt-10 grid grid-cols-2 gap-4 md:mt-6 md:gap-6 lg:mt-8 lg:grid-cols-3 xl:gap-8">
           <div className="flex flex-col items-start justify-start space-y-3 md:space-y-4">
