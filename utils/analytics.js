8cfd9c274483babdfbf40047eeac6c1382ed915a
Philani Ncube <ncbphi001@gmail.com>
2022-05-07 13:02:07 +0200

    fixed analytics

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,9 +1,10 @@
 import Analytics from 'analytics'
+import googleTagManager from '@analytics/google-tag-manager'
 
 const GTM_CONTAINER_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
 
 const analytics = Analytics({
-  app: 'hotwater24', // Call this whatever you like.
+  app: 'vuyolwethu_aluminium', // Call this whatever you like.
   plugins: [
     googleTagManager({
       containerId: GTM_CONTAINER_ID,
