9dadb0c0efb4b11fc661e3dfcfd78703e5463ffd
Philani Ncube <ncbphi001@gmail.com>
2022-06-07 18:38:40 +0200

    verification meta tag

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,6 +11,7 @@ export default function Home() {
         <meta name="description" content="Manufacturing and installation of aluminium windows, doors, security gates and burglar bars"/>
         <meta name="keywords" content="aluminium windows, doors, security gates, building, painting, paving"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-site-verification" content="SU6T1SAfsF3ZDzExyFJTw-2gQfo6ODLZ_2dPDcpfGHg" />
         <link rel="canonical" href="https://www.vuyolwethubaytrading.co.za" />
       </Head>
       <Video />
